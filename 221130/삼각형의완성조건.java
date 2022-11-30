46d785bbbe3db2e6b72b743c72321e001e97c430
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-11-30 17:16:15 +0900

    Create 삼각형의완성조건.java
    20221130 삼각형의완성조건

---
+++ 221130/삼각형의완성조건.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        int memory = 0;
+        int max=sides[0];
+        int sum=0;
+        
+        for(int i=1;i<3;i++){ //가장 긴 변의 값과 인덱스 저장
+                if(max<sides[i]){
+                    max = sides[i];
+                    memory = i;
+                }
+        }
+        
+        for(int i=0;i<3;i++){ //가장 긴 변을 제외한 나머지 두 변의 합
+            if(memory == i) continue;
+            
+            sum += sides[i];    
+        }
+        
+        if(sum>max){
+            answer = 1;
+        }else {
+            answer = 2;
+        }
+        
+        return answer;
+    }
+}
