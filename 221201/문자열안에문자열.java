bc506ffac0ea6a3c96a037e003ca0516ebab1e50
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-01 14:34:07 +0900

    Create 문자열안에문자열.java
    20221201 문자열안에문자열

---
+++ 221201/문자열안에문자열.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String str1, String str2) {     
+        if(str1.contains(str2)) return 1; //contains 함수를 통해 포함되어있는지 확인
+        else return 2;
+
+    }
+}
