c192d3198c76351cbd08115c2a48fc0e09a22bdc
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-02 15:31:28 +0900

    Update 문자열정렬하기.java

--- 221202/문자열정렬하기.java
+++ 221202/문자열정렬하기.java
@@ -25,7 +25,7 @@ class Solution {
         return answer;
       
       //return Arrays.stream(my_string.replaceAll("[A-Z|a-z]","").split("")).sorted().mapToInt(Integer::parseInt).toArray();
-      //Arrays의 stream을 통해 replaceAll로 문자들을 빈공간으로 대체하고 sorted를 통해 정렬 후 mapToInt로 정수로 변환한 후 배열로 반환
+      //Arrays의 stream을 통해 replaceAll로 문자들을 빈공간으로 대체하고 split을 통해 하나하나 잘라준다. 그  sorted를 통해 정렬 후 mapToInt로 정수로 변환한 후 배열로 반환
       
       //my_string = my_string.replaceAll("[^0-9]",""); //my_string에 0~9 빼고 다 빈 공간으로 대체
     }
