2d4e74dc1ced5992266e13df5b137d343780c385
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-02 17:18:47 +0900

    Create 약수구하기.java
    20221202 약수구하기

---
+++ 221202/약수구하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public List solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        int[] answer = {};
+        
+        for(int i=1;i<=n;i++){
+            if(n % i == 0){
+                list.add(i);
+            }
+        }
+        
+        return list;
+    }
+}
