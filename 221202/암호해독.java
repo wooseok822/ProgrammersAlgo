3f40711fcac03f73c7af1042cf0ed78b5ac9feb9
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-02 17:48:38 +0900

    Update 암호해독.java

--- 221202/암호해독.java
+++ 221202/암호해독.java
@@ -4,6 +4,7 @@ class Solution {
         
         for(int i=code;i<=cipher.length();i+=code){
             answer += cipher.charAt(i-1);
+            //answer += cipher.substring(i-1,i); //substring을 통해 자르는 방법
         }
         return answer;
     }
