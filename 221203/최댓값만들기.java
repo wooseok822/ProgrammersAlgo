4dc68a7f90975124bdaef05e3a260d832cadd0c2
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-03 16:26:21 +0900

    Create 최댓값만들기.java
    20221203 최댓값만들기

---
+++ 221203/최댓값만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=0;i<numbers.length;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                // if(max < numbers[i] * numbers[j]){
+                //     max = numbers[i] * numbers[j];
+                // }
+                max = Math.max(max, numbers[i] * numbers[j]);
+            }
+        } 
+        return max;
+    }
+}
