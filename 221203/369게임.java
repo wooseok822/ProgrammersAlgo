2056f1637307aadd37ba3e9aa1c1a9b1d8986654
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-03 18:13:48 +0900

    Update 369게임.java

--- 221203/369게임.java
+++ 221203/369게임.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 class Solution {
     public int solution(int order) {
         int answer = 0;
+        //String str_order = "" + order; //""를 더해도 문자열로 변환이 가능하다.
         
         String str_order = String.valueOf(order);
         for(int i=0;i<str_order.length();i++){
@@ -11,5 +14,8 @@ class Solution {
             }
         }
         return answer;
+        
+        //return (int) Arrays.stream(String.valueOf(order).split("")).map(Integer::parseInt).filter(i->i==3||i==6||i==9).count();
+        //order값을 문자열로 바꾸고 split으로 잘라서 Arrays.stream으로 배열로 바꾸고 map을 통해 배열 하나하나 정수로 바꾼 것을 필터로 걸러준 후 count를 센다.
     }
 }
