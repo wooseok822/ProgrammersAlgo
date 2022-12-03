baedc27b397b7f650167fc7ccac89b43dceb2152
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-03 20:09:06 +0900

    Create 중복된문자제거.java
    20221203 중복된문자제거

---
+++ 221203/중복된문자제거.java
@@ -0,0 +1,30 @@
+class Solution {  
+    public String solution(String my_string) {
+         String answer = "";
+        
+//         for(int i=0;i<my_string.length();i++){
+//             int flag = 0;
+//             for(int j=0;j<answer.length();j++){
+//                 if(answer.charAt(j) == my_string.charAt(i)) {
+//                     flag = 1;
+//                     break;
+//                 }
+//                 else flag = 0;
+//             } 
+//                 if(flag == 0) {
+//                     answer += my_string.charAt(i);
+//                 }
+//         }
+//         return answer;
+        for(int i=0;i<my_string.length();i++){
+            if(!answer.contains(String.valueOf(my_string.charAt(i)))){ // 포함되어있지 않으면 추가
+                answer += my_string.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
+
+
+
+
