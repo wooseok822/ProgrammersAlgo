ba14dc80c012337b75132ef9756c5068632cb577
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-05 15:30:23 +0900

    Update A로B만들기.java
    20221205 A로B만들기

--- 221205/A로B만들기.java
+++ 221205/A로B만들기.java
@@ -1,24 +1,39 @@
+// class Solution {
+//     public int solution(String before, String after) {
+//         int answer = 1;
+//         int[] before_count = new int[26];
+//         int[] after_count = new int[26];
+        
+//         for(int i=0;i<before.length();i++){
+//             before_count[before.charAt(i)%97]++;
+//         }
+        
+//         for(int i=0;i<after.length();i++){
+//             after_count[after.charAt(i)%97]++;
+//         }
+        
+//         for(int i=0;i<26;i++){
+//             if(before_count[i] != after_count[i]){
+//                 answer = 0;
+//                 break;
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
+import java.util.Arrays;
+
 class Solution {
     public int solution(String before, String after) {
-        int answer = 1;
-        int[] before_count = new int[26];
-        int[] after_count = new int[26];
+        int answer = 0;
+        char[] befores = before.toCharArray(); // char 배열로 변환
+        char[] afters = after.toCharArray();
         
-        for(int i=0;i<before.length();i++){
-            before_count[before.charAt(i)%97]++;
-        }
+        Arrays.sort(befores); // 배열 정렬
+        Arrays.sort(afters);
         
-        for(int i=0;i<after.length();i++){
-            after_count[after.charAt(i)%97]++;
-        }
-        
-        for(int i=0;i<26;i++){
-            if(before_count[i] != after_count[i]){
-                answer = 0;
-                break;
-            }
-        }
-        
-        return answer;
+        return new String(befores).equals(new String(afters)) ? 1 : 0; // new String으로 문자열로 만들고 비교
     }
 }
