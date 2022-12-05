ee4becdd2f0806941b2a6f1291eb6e60631a0c6a
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-05 18:27:24 +0900

    Update 이진수더하기.java

--- 221205/이진수더하기.java
+++ 221205/이진수더하기.java
@@ -1,6 +1,7 @@
 class Solution {
     public String solution(String bin1, String bin2) {      
         return Integer.toString(Integer.parseInt(bin1,2)+Integer.parseInt(bin2,2),2);
+        //     Integer.toBinaryString을 써도된다.
     } 
 }
 
