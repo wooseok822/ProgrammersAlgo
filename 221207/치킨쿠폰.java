d2290aaf26286fa2a86ba6133ac043e4fed34820
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-07 17:18:25 +0900

    Create 치킨쿠폰.java
    20221207 치킨쿠폰

---
+++ 221207/치킨쿠폰.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+        
+        while (chicken >= 10){
+            int service = chicken / 10;
+            int remain = chicken % 10;
+            
+            answer += service;
+            
+            chicken = service + remain; 
+            
+        }
+
+        return answer;
+    }
+}
