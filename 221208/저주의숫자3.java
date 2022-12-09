783f7c343667b910a7f0fa6e7408145adb5bb5bf
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-09 15:09:20 +0900

    Update 저주의숫자3.java

--- 221208/저주의숫자3.java
+++ 221208/저주의숫자3.java
@@ -4,8 +4,8 @@ class Solution {
         int[] num = new int[101];
         int idx = 1;
         int i = 1;
-        while (i <= 100){ // 배열에 값을 다 넣음
-            if(idx % 3 == 0 || include(idx) == 1) {  // 3의 배수거나 3이 포함되어있으면 idx를 올리고 반복문 다시 실행
+        while (i <= n){
+            if(idx % 3 == 0 || String.valueOf(idx).contains("3")) { // 3으로 나누어지거나 3이 포함되어있는지 확인
                 idx++;
                 continue;
             }
@@ -14,11 +14,5 @@ class Solution {
         answer = num[n];
         return answer;
     }
-    
-    public int include(int number){   // 3이 포함되어있는지 확인
-        String num_str = String.valueOf(number);
-        
-        if(num_str.contains("3")) return 1;
-        else return 0;
-    }
+
 }
