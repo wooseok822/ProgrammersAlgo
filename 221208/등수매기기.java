e7b231c9ad942887eb2f62f3ff5d5e6615583b77
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-09 14:23:26 +0900

    Update 등수매기기.java

--- 221208/등수매기기.java
+++ 221208/등수매기기.java
@@ -1,21 +1,43 @@
+// class Solution {
+//     public int[] solution(int[][] score) {
+//         int[] answer = new int[score.length];
+//         int[] sum = new int[score.length];
+        
+//         for(int i=0;i<score.length;i++){
+//             sum[i] = score[i][0] + score[i][1];
+//         }
+        
+//         for(int i=0;i<sum.length;i++){
+//             int cnt = 1;
+//             for(int j=0;j<sum.length;j++){
+//                 if(sum[i]<sum[j]){
+//                     cnt++;
+//                 }
+//             }
+//             answer[i] = cnt;
+//         }        
+        
+//         return answer;
+//     }
+// }
+
+import java.util.*;
+
 class Solution {
     public int[] solution(int[][] score) {
-        int[] answer = new int[score.length];
-        int[] sum = new int[score.length];
+        List<Integer> list = new ArrayList<>();
         
-        for(int i=0;i<score.length;i++){
-            sum[i] = score[i][0] + score[i][1];
+        for(int[] t : score){   // for-each 문으로 반복문
+            list.add(t[0] + t[1]);
         }
+        list.sort(Comparator.reverseOrder());   // 리스트를 정렬한다. Comparator.reverseOrder()를 통해 내림차순
         
-        for(int i=0;i<sum.length;i++){
-            int cnt = 1;
-            for(int j=0;j<sum.length;j++){
-                if(sum[i]<sum[j]){
-                    cnt++;
-                }
-            }
-            answer[i] = cnt;
-        }        
+        int[] answer = new int[score.length];
+        
+        for(int i=0;i<score.length;i++){        
+            answer[i] = list.indexOf(score[i][0] + score[i][1]) + 1;    // indexOf를 사용하면 가장 적은 인덱스가 나오므로 
+        }
+            
         
         return answer;
     }
