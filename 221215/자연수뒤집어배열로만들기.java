cd48861f74f78305b4ceb6c167b6bc2028c060e5
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-16 14:12:10 +0900

    Update 자연수뒤집어배열로만들기.java

--- 221215/자연수뒤집어배열로만들기.java
+++ 221215/자연수뒤집어배열로만들기.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int[] solution(long n) {     
+        String[] num = String.valueOf(n).split("");       
+        int[] answer = new int[num.length];
+        int idx = num.length - 1;
+        
+        for(int i=0;i<num.length;i++){
+            answer[i] = Integer.parseInt(num[idx--]);
+        }
+        
+        return answer;
+    }
+}
