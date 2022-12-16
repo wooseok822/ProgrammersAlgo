9a862d03f0392a96f2523053dd2f25ccdcdf888f
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-16 16:53:52 +0900

    Update 흉부외과또는일반외과의사목록출력하기.sql

--- 221216/흉부외과또는일반외과의사목록출력하기.sql
+++ 221216/흉부외과또는일반외과의사목록출력하기.sql
@@ -1,1 +1,8 @@
+SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD   
+FROM DOCTOR
+WHERE MCDP_CD = 'CS' OR MCDP_CD = 'GS'
+ORDER BY HIRE_YMD DESC, DR_NAME ASC;
 
+# DATE_FORMAT(날짜, '포맷')
+# %Y => 4글자 년도   %y => 2글자 년도    %m => 2글자 월      %d => 2글자 일   %c => 1글자 월
+# %M => 월 영문      %H => 24시간 시간   %l => 12시간 시간   %i => 분         %S => 초
