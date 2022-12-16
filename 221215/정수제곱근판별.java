319bfdf59dd3cf414a0ae66e4afc2bdb32e5707a
wooseok822 <76737870+wooseok822@users.noreply.github.com>
2022-12-16 14:33:35 +0900

    Update 정수제곱근판별.java

--- 221215/정수제곱근판별.java
+++ 221215/정수제곱근판별.java
@@ -1,1 +1,11 @@
-정수제곱
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double sqrt_n = Math.sqrt(n);   //sqrt와 pow는 double형으로 반환됨.
+        if(n % sqrt_n == 0){
+            answer = (long)Math.pow(sqrt_n+1, 2);
+        }else answer = -1;
+        
+        return answer;
+    }
+}
